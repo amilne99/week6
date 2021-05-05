98d729e16bf614f5e1beb230713257244359e867
Andrew Milne <Andrew.milne@kellogg.northwestern.edu>
2021-05-05 10:51:20 -0500

    week6 code alongs

--- netlify/functions/reddit.js
+++ netlify/functions/reddit.js
@@ -12,28 +12,41 @@ exports.handler = async function(event) {
   console.log(event)
 
   // Get the parameter for which subreddit
-
+  let subreddit = event.queryStringParameters.subreddit
+  console.log(subreddit)
   // define the URL for Reddit posts data
-  // let url = `https://feed2json.org/convert?url=https%3A%2F%2Fwww.reddit.com%2Fr%2F${subreddit}%2F.rss`
-
+  let url = `https://feed2json.org/convert?url=https%3A%2F%2Fwww.reddit.com%2Fr%2F${subreddit}%2F.rss`
+  
   // - Fetch the url, wait for a response, store the response in memory
-
+  let response = await fetch(url)
+  console.log(response)
   // - Ask for the json-formatted data from the response, wait for the data, store it in memory
+  let json = await response.json()
 
   // - Write the json-formatted data to the back-end console
-
+  console.log(json)
+  console.log(json.items.length)
   // Create a new Array to be returned by the API
-
+  let returnArray = []
   // Loop through the posts, for each one:
+  for (i=0; i<json.items.length;i++){
 
     // Store each post from the Reddit API in memory
-
+    let post = json.items[i]
     // Create a new post object containing the pertinent fields
+    postObject = {
+      url: post.url,
+      title: post.title,
+      datePublished: post.date_published,
+      author: post.author.name
+    }
 
+    // Return only the URL, title, date published, and author of each post.
     // Add (push) the post object to the final Array
-
+    returnArray.push(postObject)
+  }
   return {
     statusCode: 200,
-    body: `Hello from the back-end!` // this must be a String
+    body: JSON.stringify(returnArray) // this must be a String
   }
 }
